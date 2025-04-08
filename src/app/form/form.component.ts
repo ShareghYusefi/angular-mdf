ed0fbb00280379cf83161522e572772bc0536428
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-04-07 18:30:33 -0600

    setup bootstrap & form

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form',
+  standalone: false,
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+export class FormComponent {}
