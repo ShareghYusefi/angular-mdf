fdaccfc585df96283f8762b64e458373135b0c44
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-04-07 19:19:30 -0600

    reactive form with FormGroup/FormControl

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'reactive-form',
@@ -9,16 +9,19 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ReactiveFormComponent {
   // ! means that the variable will be initialized later
-  reactiveForm!: FormGroup;
+  reactiveForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   // We can use a FormBuilder instance via dependency injection to create a form group
-  constructor(private formBuilderInstance: FormBuilder) {
-    // Create a form group with two form controls: name and email
-    this.reactiveForm = this.formBuilderInstance.group({
-      email: '',
-      password: '',
-    });
-  }
+  // constructor(private formBuilderInstance: FormBuilder) {
+  //   // Create a form group with two form controls: name and email
+  //   this.reactiveForm = this.formBuilderInstance.group({
+  //     email: '',
+  //     password: '',
+  //   });
+  // }
 
   onSubmit() {
     console.log(this.reactiveForm.value);
